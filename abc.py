54d05604aebdb72e1c0a8ce3820348b9ca7c76c8
brenjini <brenjini@gmail.com>
2023-07-02 16:30:17 -0700

    Update abc.py

--- abc.py
+++ abc.py
@@ -9,7 +9,19 @@ def run():
     st.image(img1, use_column_width=False)
     st.title("Credit card Prediction using Machine Learning")
 
-    st.tabs('First','Second','Third')
+    tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+   with tab1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+
+   with tab2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+   with tab3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
 
     ## Account No
     account_no = st.text_input('Account Number')
