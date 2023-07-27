273748e3009292e0bb81288f68ea0bc1edab750e
brenjini <brenjini@gmail.com>
2023-07-26 20:07:41 -0700

    Update abc.py

--- abc.py
+++ abc.py
@@ -26,8 +26,8 @@ EDUCATION_TYPE_INCOMPLETE_HIGHER_EDU = False
 EDUCATION_TYPE_LOWER_SECONDARY = False
 FAMILY_STATUS_CIVIL_MARRIAGE = False
 FAMILY_STATUS_MARRIED = False
-FAMILY_STATUS_SEPARATED = False
 FAMILY_STATUS_SINGLE = False
+FAMILY_STATUS_SEPARATED = False
 HOUSING_TYPE_SHARE_APT = False
 HOUSING_TYPE_HOUSE_APT = False
 HOUSING_TYPE_MUNICIPAL_APT = False
@@ -67,7 +67,7 @@ options = list(range(len(education_type)))
 education = st.selectbox("Type of Education", options, format_func=lambda x: education_type[x])
 
 # FAMILY_STATUS(12-15)
-family_status = ('Civil marriage', 'Single / not married', 'Separated', 'Widow')
+family_status = ('Civil marriage', 'Married', 'Single / not married', 'Separated')
 options = list(range(len(family_status)))
 family = st.selectbox("Family Status", options, format_func=lambda x: family_status[x])
 
@@ -132,6 +132,15 @@ if st.button("Submit"):
     else:
        EDUCATION_TYPE_LOWER_SECONDARY = True
 ####################################
+    if(family == 0):
+       FAMILY_STATUS_CIVIL_MARRIAGE = True
+    elif(family == 1):
+       FAMILY_STATUS_MARRIED = True
+    elif(family == 2):
+       FAMILY_STATUS_SINGLE = True
+    else:
+       FAMILY_STATUS_SEPARATED = True
+####################################
     if(housing == 0):
        HOUSING_TYPE_SHARE_APT = True
     elif(housing == 1):
@@ -144,9 +153,10 @@ if st.button("Submit"):
        HOUSING_TYPE_RENTED_APT = True
 
 
-    
+
+    input_data =(AMT_INCOME_TOTAL, FLAG_WORK_PHONE, FLAG_LAND_PHONE, YEARS_OF_EMPLOYMENT, CODE_GENDER_F, FLAG_OWN_REALTY, INCOME_TYPE_COMM_ASSOCIATE, INCOME_TYPE_PENSIONER, INCOME_TYPE_STATE_SERVANT, INCOME_TYPE_STUDENT, EDUCATION_TYPE_HIGHER_EDU, EDUCATION_TYPE_ACADEMIC_DEGREE, EDUCATION_TYPE_INCOMPLETE_HIGHER_EDU, EDUCATION_TYPE_LOWER_SECONDARY, FAMILY_STATUS_CIVIL_MARRIAGE, FAMILY_STATUS_MARRIED, FAMILY_STATUS_SINGLE, FAMILY_STATUS_SEPARATED, HOUSING_TYPE_SHARE_APT, HOUSING_TYPE_HOUSE_APT, HOUSING_TYPE_MUNICIPAL_APT, HOUSING_TYPE_OFFICE_APT, HOUSING_TYPE_RENTED_APT)    
     #input_data =(315000.0, 0, 0, 2, True, False, True, False, False, False, False, True, False, False, False, True, False, False, False, True, False, False, False)
-    input_data =(157500.0, 0, 1, 3, True, False, False, True, False, False, False, True, False, False, False, True, False, False, False, True, False, False, False)
+    #input_data =(157500.0, 0, 1, 3, True, False, False, True, False, False, False, True, False, False, False, True, False, False, False, True, False, False, False)
     input_data_as_numpy_array = np.asarray(input_data)
     input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
     prediction = loaded_model.predict(input_data_reshaped)
