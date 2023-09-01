752b9954ba6e33f5613dcb2ab05044c3925255da
brenjini <137976299+brenjini@users.noreply.github.com>
2023-08-31 22:35:17 -0700

    Add files via upload

---
+++ 123.py
@@ -0,0 +1,15 @@
+import numpy as np
+import streamlit as st
+from PIL import Image
+import pickle
+
+loaded_model = pickle.load(open('trained_model.sav', 'rb'))
+
+def run():
+    st.title("Credit Card Approval Prediction using Machine Learning")
+    st.sidebar.header('Application Form')
+    img1 = Image.open('images.jpg')
+    st.image(img1, use_column_width=False)
+
+run()
+
